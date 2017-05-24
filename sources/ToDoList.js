9ec54b05dd5aaa042a470ec49102b62f78661fd6
ZloiY <NoRFolf1000@gmail.com>
2017-05-24 12:25:01 +0300

    comment about hot keys

--- sources/ToDoList.js
+++ sources/ToDoList.js
@@ -19,34 +19,34 @@ class ToDoList{
         checkUncheckBtn.addEventListener('click', operations.setCheckUnCheckAll, false);
         delCheckedBtn.addEventListener('click', operations.delCheckEl, false);
         inputField.onkeyup = enterDelPressed => {
-            if (enterDelPressed.keyCode === 13)
+            if (enterDelPressed.keyCode === 13)  // press ENTER to add item
                 operations.addElement();
-            if (enterDelPressed.keyCode === 46)
+            if (enterDelPressed.keyCode === 46) // press Del to clear input field
                 inputField.value = '';
         };
         document.onkeyup = hotKeyPressed => {
-            if (document.activeElement !== inputField)
+            if (document.activeElement !== inputField) // hot keys below will work if input field  wouldn't  be selected
                 switch (hotKeyPressed.keyCode){
-                    case 65:{
+                    case 65:{                                   //press A to display all items
                         operations.displayAllItems();
                         allRadio.checked = true;
                         break;
                     }
-                    case 67:{
+                    case 67:{                                 // press C to display complete items
                         operations.displayCompleteItems();
                         completeRadio.checked = true;
                         break;
                     }
-                    case 86:{
+                    case 86:{                               // press V to display active items
                         operations.displayItemsInProgress();
                         activeRadio.checked = true;
                         break;
                     }
-                    case 83:{
+                    case 83:{                              // press S to check/uncheck all items
                         operations.setCheckUnCheckAll();
                         break;
                     }
-                    case 68:{
+                    case 68:{                            // press D to delete all checked items
                         operations.delCheckEl();
                         break;
                     }
