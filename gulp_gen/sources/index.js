1a433e39c3f2c33f24b367149ff01b98011ffb88
ZloiY <NoRFolf1000@gmail.com>
2017-06-09 17:49:53 +0300

    gulp build and scss

--- gulp_gen/sources/index.js
+++ gulp_gen/sources/index.js
@@ -240,3 +240,95 @@ function radioGroupLabelsClicked(radioGroupLabel) {
     }
   }
 }
+
+window.addEventListener('load', (loadEvent) => {
+  main();
+});
+
+function main() {
+  const operations = new ItemListOperations();
+  operations.displayAllItems();
+  addBtnsFieldEvListeners(operations);
+  addDocHotKeyEvListeners(operations);
+  addInputFieldEvListener(operations);
+}
+
+function  addBtnsFieldEvListeners(operations) {
+  $('#addBtn').click(operations.addElement);
+  $('#all').click(operations.displayAllItems);
+  $('#complete').click(operations.displayCompleteItems);
+  $('#active').click(operations.displayItemsInProgress);
+  $('#checkUncheck').click(operations.setCheckUnCheckAll);
+  $('#delBtn').click(operations.delCheckEl);
+  $('#allTasks').click(operations.displayAllItems);
+  $('#completeTasks').click(operations.displayCompleteItems);
+  $('#activeTasks').click(operations.displayItemsInProgress);
+}
+
+function addInputFieldEvListener(operations) {
+  $('#itemInput').keyup((enterDelPressed) => {
+    enterKeyPressed(enterDelPressed.keyCode, operations);
+    delKeyPressed(enterDelPressed.keyCode);
+  });
+ }
+
+function addDocHotKeyEvListeners(operations) {
+  $(document).keyup((hotKeyPressed) => {
+    addHotKeyListeners(hotKeyPressed.keyCode, operations);
+  });
+}
+
+function addHotKeyListeners(hotKeyCode, operations) {
+  if (!$('#itemInput').is(':focus')) {
+    hotKeyAPressed(hotKeyCode, operations);
+    hotKeyCPressed(hotKeyCode, operations);
+    hotKeyVPressed(hotKeyCode, operations);
+    hotKeyDPressed(hotKeyCode, operations);
+    hotKeySPressed(hotKeyCode, operations);
+  }
+}
+
+function enterKeyPressed(enterKeyCode, operations) {
+  if (enterKeyCode === 13) {
+    operations.addElement();
+  }
+}
+
+function delKeyPressed(delKeyCode) {
+  if (delKeyCode === 46) {
+    $('#itemInput').val('');
+  }
+}
+
+function hotKeyAPressed(hotKeyCode, operations) {
+  if (hotKeyCode === 65) {
+    operations.displayAllItems();
+    $('#all').val(true);
+  }
+}
+
+function hotKeyCPressed(hotKeyCode, operations) {
+  if (hotKeyCode === 67) {
+    operations.displayCompleteItems();
+    $('#complete').val(true);
+    }
+}
+
+function hotKeyVPressed(hotKeyCode, operations){
+  if (hotKeyCode === 86) {
+    operations.displayItemsInProgress();
+    $('#active').val(true);
+   }
+}
+
+function hotKeySPressed(hotKeyCode, operations) {
+  if (hotKeyCode === 83) {
+    operations.setCheckUnCheckAll();
+  }
+}
+
+function hotKeyDPressed(hotKeyCode, operations) {
+  if (hotKeyCode === 68) {
+    operations.delCheckEl();
+  }
+}
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var csso = require('gulp-csso');
+var composer = require('gulp-uglify/composer');
+var uglify_es = require('uglify-es');
+var minify = composer(uglify_es, console);
+var concat = require('gulp-concat');
+var connect = require('connect');
+var livereload = require('gulp-livereload');
+var pump = require('pump');
+var lr = require('tiny-lr');
+var server = lr();
+
+gulp.task('css', function () {
+  gulp.src('./gen_style/style.css')
+    .pipe(csso())
+    .pipe(gulp.dest('./gulp_gen/style/'))
+    .pipe(livereload(server));
+});
+
+gulp.task('js', function() {
+  gulp.src('./sources/*.js')
+    .pipe(concat('index.js'))
+    .pipe(gulp.dest('./gulp_gen/sources/'))
+    .pipe(livereload(server));
+});
+
+gulp.task('http-server', function() {
+  connect()
+    .use(require('connect-livereload')())
+    .use(connect.static('./gulp_gen'))
+    .listen('9000');
+  console.log('Server listening on http://localhost:9000');
+});
+
+gulp.task('watch', function() {
+  gulp.run('css');
+  gulp.run('js');
+
+  server.listen(35729, function(err) {
+    if (err) return console.log(err);
+    gulp.watch('gen_style/style.css', function() {
+      gulp.run('css');
+    });
+    gulp.watch('sources/*.js', function() {
+      gulp.run('js');
+    });
+  });
+  //gulp.run('http-server');
+});
+
+gulp.task('build', function() {
+
+  pump([
+    gulp.src('./gen_style/style.css'),
+    csso(),
+    gulp.dest('./gulp_build/style/')
+  ]);
+  var options = {};
+  pump([
+    gulp.src('./sources/*.js'),
+    concat('index.js'),
+    gulp.dest('./gulp_gen/sources'),
+    minify(options).on('error', function (e) {
+      console.log(e);
+    }),
+    gulp.dest('./gulp_build/sources/')
+  ]);
+});
